f0a41a4c1934e07e5aca8cb6c142cff0fce5710e
ashwininalavade <ashunalavade26@gmail.com>
2026-07-15 14:11:04 +0530

    create user mutation added | prisma setup completed

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@as-integrations/express5';
+import { prismaClient } from './lib/prisma.js';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 8000;
@@ -13,12 +14,38 @@ const gqlServer = new ApolloServer({
         hello: String
         say(name: String): String
     }
+
+    type Mutation {
+      createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+    }
   `,
   resolvers: {
     Query: {
         hello: () => `Hey, I am a graphql server`,
         say: (_, {name}: {name: string}) => `Hey ${name}, How are you?`
-    }
+    },
+    Mutation: {
+      createUser: async(
+        _, 
+        { 
+          firstName, 
+          lastName, 
+          email, 
+          password,
+        } : {
+          firstName: string; 
+          lastName: string;
+          email: string; 
+          password: string;
+        }) => {
+            await prismaClient.user.create({
+              data: {
+                firstName, lastName, email, password, salt: 'random_salt'
+              }
+          });
+          return true;
+        },
+      }
   },
 });
 
